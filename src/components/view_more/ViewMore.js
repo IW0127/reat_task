fd8a601aced1c1546d898566117e2db533db403d
hardikIdentix <hardik.dholariya@elookinto.org>
2022-07-28 17:48:44 +0530

    view more

--- src/components/view_more/ViewMore.js
+++ src/components/view_more/ViewMore.js
@@ -1,46 +1,48 @@
 import React, { useEffect, useState } from 'react';
 
 const ViewMore = () => {
-  // const viewMoreNumber = [1, 2, 6, 8, 7, 9];
-  const [viewMore, setViewMore] = useState([1, 2, 6, 8, 7, 9]);
-  const [viewCount, setViewCount] = useState(0);
-  const [search, setSearch] = useState(0);
+  // view more values
+  const viewMore = [1, 2, 6, 8, 7, 9];
 
+  //count view button
+  const [viewCount, setViewCount] = useState(0);
+
+  const [search, setSearch] = useState(null);
+  let [viewNumber, setViewNumber] = useState(0);
   const increment = () => {
     setSearch((prev) => prev + 1);
   };
 
   const decrement = () => {
-    if (count > 0) {
-      setSearch((prev) => prev - 1);
-    }
+    if (search > 0) setSearch((prev) => prev - 1);
   };
 
   const changeOnSearch = (e) => {
-    const searchValue = e.target.value;
-    setSearch(searchValue);
+    const searchValue = Number(e.target.value);
+    searchValue ? setSearch(searchValue) : setSearch(0);
   };
+  const clickNumber = (e) => {
+    const numId = Number(e.target.dataset.id);
+    setSearch(numId);
+  };
+
+  useEffect(() => {
+    const index = viewMore.indexOf(search);
+    if (index >= 0) {
+      setViewCount(index + 2);
+      setViewNumber(index);
+    } else {
+      setViewCount(null);
+      setViewNumber(0);
+    }
+  }, [search]);
 
   const onClickViewMore = () => {
     setViewCount((prev) => prev + 2);
   };
 
-  useEffect(() => {
-    const viewMoreArray = viewMoreNumber.slice(0, viewCount);
-    setViewMore(() => viewMoreArray);
-  }, [viewCount]);
-
-  const resetState = () => {
-    setViewCount(() => 0);
-    setCount(() => 0);
-    setViewMore(() => []);
-    setSearch(() => 0);
-  };
   return (
     <div className='container text-center align-items-center d-flex justify-content-center my-3 flex-column'>
-      <button className='btn btn-primary m-4' onClick={resetState}>
-        Rest
-      </button>
       <div className='input-group' style={{ width: '15%' }}>
         <button
           className='btn btn-primary'
@@ -53,7 +55,7 @@ const ViewMore = () => {
 
         <input
           type='text'
-          value={search}
+          value={search ?? 0}
           onChange={changeOnSearch}
           className='form-control text-center'
         />
@@ -67,13 +69,29 @@ const ViewMore = () => {
         </button>
       </div>
       <div className='my-3 p-3 rounded-8 w-25 bg-primary bg-opacity-25'>
-        {viewMore.map((num, id) => (
-          <strong key={id} className='text-dark'>
-            {num}
-            <br />
-          </strong>
-        ))}
-        <button className='btn btn-primary my-1' onClick={onClickViewMore}>
+        {viewMore.map((num, id) => {
+          if (id < viewCount && viewNumber <= id) {
+            return (
+              <strong
+                key={id}
+                onClick={clickNumber}
+                data-id={num}
+                className='text-dark'
+              >
+                {num}
+                <br />
+              </strong>
+            );
+          }
+        })}
+        <button
+          className='btn btn-primary my-1'
+          onClick={onClickViewMore}
+          disabled={
+            viewCount === viewMore.length ||
+            search >= viewMore[viewMore.length - 1]
+          }
+        >
           View More
         </button>
       </div>
